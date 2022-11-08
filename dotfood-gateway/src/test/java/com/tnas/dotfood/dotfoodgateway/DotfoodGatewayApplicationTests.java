74156ddeae90fbf0901f50d9eb92bbdadbfef6da
Thiago Nascimento <nascimenthiago@gmail.com>
2022-11-08 14:53:48 -0300

    Adding gateway

---
+++ dotfood-gateway/src/test/java/com/tnas/dotfood/dotfoodgateway/DotfoodGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnas.dotfood.dotfoodgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotfoodGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
